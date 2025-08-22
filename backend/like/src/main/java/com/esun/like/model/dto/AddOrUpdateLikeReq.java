4e0cb9fce05131cfacf8aef5996d089c65e3b8f9
tsai <zoe89418@gmail.com>
2025-08-22 15:07:48 +0800

    backend: add DTO AddOrUpdateLikeReq

---
+++ backend/like/src/main/java/com/esun/like/model/dto/AddOrUpdateLikeReq.java
@@ -0,0 +1,21 @@
+package com.esun.like.model.dto;
+
+import lombok.Data;
+import jakarta.validation.constraints.*;
+
+@Data
+public class AddOrUpdateLikeReq {
+    @NotBlank
+    private String userId;
+
+    
+    @NotNull
+    private Long productNo;
+
+    @NotBlank
+    private String account;
+
+    @NotNull
+    @Positive
+    private Integer order name;
+}
