7071246da3aeb9e34815d967b34b4de3df47cb05
tsai <zoe89418@gmail.com>
2025-08-23 03:06:45 +0800

    backend: add ProductService (create, list)

---
+++ backend/like/src/main/java/com/esun/like/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.esun.like.service;
+
+import com.esun.like.model.dto.AddProductReq;
+import com.esun.like.model.dto.ProductDto;
+import com.esun.like.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository repo;
+
+    @Transactional
+    public long create(AddProductReq req) {
+        return repo.create(req);
+    }
+
+    public List<ProductDto> list() {
+        return repo.list();
+    }
+}
